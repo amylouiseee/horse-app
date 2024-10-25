a742e4c18c23b42a232f7d76747b2567a7f01c2e
amylouiseee <ameh456@gmail.com>
2024-10-25 12:52:04 +1000

    Setting up menu
    Set up more css
    Change title
    Add a basic header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <nav className="flex space-x-4" >
+            {[
+                ['Home', '/dashboard'],
+                ['Add a Horse', '/add'],
+                ['View Horses', '/horses'],
+            ].map(([title, url]) => (
+                <a href={url} className="px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
+            ))}
+            </nav>
+            <hr />
+        </header>
+    );
+  }
+  
+  export default Header;
